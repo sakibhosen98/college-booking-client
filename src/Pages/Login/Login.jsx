e00c874f3f6dca5a28196774aa0fa9959c2c22c7
sakibhosen98 <sakibhosen9815@gmail.com>
2023-07-24 19:34:18 +0600

    added first commit

---
+++ src/Pages/Login/Login.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+import image from '../../assets/images/banner/banner1.jpg';
+
+
+const Login = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+    const [loginError, setLoginError] = useState('');
+
+
+
+
+    const handleLogin = (data) => {
+        console.log(data);
+        // console.log(errors.password);
+        // setLoginError('');
+        // signUser(data.email, data.password)
+        //     .then(result => {
+
+        //         console.log(result.user);
+        //         setLoginUserEmail(data.email);
+        //     })
+        //     .catch(error => {
+        //         console.log(error.message);
+        //         setLoginError(error.message)
+        //     })
+    }
+
+    const handleGoogleLogin = () =>{
+
+        console.log('hello google user .... working ....');
+    }
+
+    return (
+        <div className="hero w-full my-20">
+            <div className="hero-content grid md:grid-cols-2 gap-10 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <img className='w-3/4 rounded' src={image} alt="login_Image" />
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-10">
+                    <h1 className="text-5xl text-center font-bold">Login</h1>
+                    <form onSubmit={handleSubmit(handleLogin)} className="card-body">
+                        <div className="form-control w-full">
+                            <label className="label"><span className="label-text text-2xl">Email</span></label>
+                            <input type='email' className="input input-bordered w-full" {...register("email", { required: "Email is required", maxLength: { value: 30, message: "Lessthen 30 character" } })}
+                            />
+                            {errors.email && <p className='text-red-600' >{errors.email?.message}</p>}
+                        </div>
+
+                        <div className="form-control w-full ">
+                            <label className="label"><span className="label-text text-2xl">Password</span></label>
+                            <input type='password' className="input input-bordered w-full " {...register("password", { required: "Password is required", minLength: { value: 6, message: "Password must be 6 character" } })}
+                            />
+
+                            <label htmlFor="forgotPassword_model" className="label"><span className="label-text">Forgot Password</span></label>
+
+                            {errors.password && <p className='text-red-600' >{errors.password?.message}</p>}
+                        </div>
+                        <input className="input input-bordered w-full btn-accent " type="submit" value="Login" />
+
+                        <div>
+                            {loginError && <p className='text-red-600' >{loginError}</p>}
+                        </div>
+
+                    </form>
+                    <p className='my-2 text-center'>New Student <Link to='/registration' className='text-secondary' >Create new account</Link>  </p>
+                    <div className='divider' >OR</div>
+                    <input onClick={handleGoogleLogin} className='btn w-full' value='CONTINUE WITH GOOGLE' ></input>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCKTfbb29IbLWOwGuNptFyR0kxyK5Vw7jg",
+  authDomain: "college-booking-68b50.firebaseapp.com",
+  projectId: "college-booking-68b50",
+  storageBucket: "college-booking-68b50.appspot.com",
+  messagingSenderId: "233712993735",
+  appId: "1:233712993735:web:5d468dd4b8fbc6cfee85e2"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
